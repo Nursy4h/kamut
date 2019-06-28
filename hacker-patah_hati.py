d91ec696e5be46be15fa573b40e60b35f49a22c4
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-06-28 22:10:48 +0700

    Update hacker-patah_hati.py

--- hacker-patah_hati.py
+++ hacker-patah_hati.py
@@ -10,15 +10,15 @@ def mengetik(s):
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
-mengetik('   #HackerPatahHati
-Girl, meskipun aku lagi belajar PHP, aku gak akan php-in kamu
-Girl, tau gak PHP itu mudah dipelajarin, tapi kamu susah dilupain 
-Girl, kalo aku ping, kenapa timenya sampe 4 angka 
-Girl, aku pakai 8080 untuk menemuimu, 25 untuk menghubungimu tapi kenapa kamu menjawab error 406
-Girl, akhirnya aku gunakan 21, tapi kenapa error 401
-Girl, apa ip aku dibanned olehmu
-Girl, aku gak akan men-DOS lamu, karena aku pakai anonymity untuk sniffing kamu
-Girl, aku cemburu saat kamu didekati pria lain, butuh nitrocooler untuk mendinginkan hatiku
-Girl, biarpun kamu tidak acuh pdaku, tapi trojanku selalu memberi tau kalo ada malware berbahaya
-Girl, akhirnya aku tau, tak ada vurnerable untuk mendapatkan hatimu
-Girl, kuputuskan delete all about you, meski kusisakan satu bookmark tentangmu ')
+mengetik('   #HackerPatahHati')
+mengetik('Girl, meskipun aku lagi belajar PHP, aku gak akan php-in kamu')
+mengetik('Girl, tau gak PHP itu mudah dipelajarin, tapi kamu susah dilupain ')
+mengetik('Girl, kalo aku ping, kenapa timenya sampe 4 angka ')
+mengetik('Girl, aku pakai 8080 untuk menemuimu, 25 untuk menghubungimu tapi kenapa kamu menjawab error 406')
+mengetik('Girl, akhirnya aku gunakan 21, tapi kenapa error 401')
+mengetik('Girl, apa ip aku dibanned olehmu')
+mengetik('Girl, aku gak akan men-DOS lamu, karena aku pakai anonymity untuk sniffing kamu')
+mengetik('Girl, aku cemburu saat kamu didekati pria lain, butuh nitrocooler untuk mendinginkan hatiku')
+mengetik('Girl, biarpun kamu tidak acuh pdaku, tapi trojanku selalu memberi tau kalo ada malware berbahaya')
+mengetik('Girl, akhirnya aku tau, tak ada vurnerable untuk mendapatkan hatimu')
+mengetik('Girl, kuputuskan delete all about you, meski kusisakan satu bookmark tentangmu ')
