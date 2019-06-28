f0df8d643d46a58784fd0e5ef39080666035d680
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-06-28 23:46:53 +0700

    Update hacker-patah-hati.py

--- hacker-patah-hati.py
+++ hacker-patah-hati.py
@@ -11,6 +11,8 @@ G = "\033[32m"    # Green
 W = "\033[0;1m"     # White
 R = "\033[31m"    # Red
 C = "\033[36;1m"    # Cyan
+
+
 import random
 import sys
 import time
@@ -23,14 +25,14 @@ def mengetik(s):
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
-mengetik(B'   #HackerPatahHati')
+mengetik(GG'   #HackerPatahHati')
 mengetik('Girl, meskipun aku lagi belajar PHP, aku gak akan php-in kamu')
 mengetik('Girl, tau gak PHP itu mudah dipelajarin, tapi kamu susah dilupain ')
 mengetik('Girl, kalo aku ping, kenapa timenya sampe 4 angka ')
 mengetik('Girl, aku pakai 8080 untuk menemuimu, 25 untuk menghubungimu tapi kenapa kamu menjawab error 406')
 mengetik('Girl, akhirnya aku gunakan 21, tapi kenapa error 401')
 mengetik('Girl, apa ip aku dibanned olehmu')
-mengetik('Girl, aku gak akan men-DOS lamu, karena aku pakai anonymity untuk sniffing kamu')
+mengetik('Girl, aku gak akan men-DOS kamu, karena aku pakai anonymity untuk sniffing kamu')
 mengetik('Girl, aku cemburu saat kamu didekati pria lain, butuh nitrocooler untuk mendinginkan hatiku')
 mengetik('Girl, biarpun kamu tidak acuh pdaku, tapi trojanku selalu memberi tau kalo ada malware berbahaya')
 mengetik('Girl, akhirnya aku tau, tak ada vurnerable untuk mendapatkan hatimu')
