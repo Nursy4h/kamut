97a9d857337aa4605955246a6bbb9d1125a1e649
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-06-28 22:56:31 +0700

    Update and rename INTRO.py to intro.py

---
+++ intro.py
@@ -0,0 +1,48 @@
+
+
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik('.           hallo teman-teman.')
+mengetik('.         Nama saya Nazri azril')
+mengetik('.            Agama saya islam                                                       ')
+mengetik('.          Asal Saya Dari Sumut')
+mengetik('.           Saya Anak Skolahan')
+mengetik('.          kegiatan saya hari hari')
+mengetik('.      Membantu Pekerjaan Org tua saya')
+mengetik('.     Ngegame(katanya sih nogame nolife) ')
+mengetik('.   Game yg sya sukai pb.ff.oceanar.wormax.  ')
+mengetik('.                                                                              ')
+mengetik('.       Status .........................                                                         ')
+mengetik('.   Pacaran dari 2016 september 26 zampai sekang                                 ')
+mengetik('.            Kalo kalian pasti jomblo kan:-D                                                       ')
+mengetik('.       Nah sekarang tentang tools ini........                                                               ')
+mengetik('.                                                                             ')                                                       ')
+mengetik('.    Kalian bisa dapatkan kata kata >>>gombalan hacker                                                              ')
+mengetik('.                                   >>>hacker patah hati                               ')
+mengetik('.                                   >>>hacker sedih                              ')
+mengetik('.                                   >>>cerita horor hacker                               ')
+mengetik('.                                   >>>dan bnyak lagi                               ')
+mengetik('.         mau tau isinya smua                                                         ')
+mengetik('.  Ketik githubnya    lalu                                                            ')
+mengetik('.    cd kamut      dan buat                                                        ')
+mengetik('.  sl untuk mengetahui kata kata yang ada di dalam tools ini                                                        ')
+
+mengetik('.    lalu ketik                                                               ')
+mengetik('.             python2 <namafoldernya>.py')
+mengetik('.          Contoh nya python2 hacker-patah-hati.py')
+
+
+
+
+
