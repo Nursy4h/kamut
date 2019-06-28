3acd543d68c8bfbe482f30d8fdd65bac42140867
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-06-29 01:39:48 +0700

    Create gombalan-hacker.py

---
+++ gombalan-hacker.py
@@ -0,0 +1,57 @@
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.2 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik(RR+' #GOMBALAN HACKER 		')													
+mengetik(GG+'				')											
+mengetik('Kalau donlot file .iso pake command 'wget -c' biar pas putus ditengah jalan bisa nyambung lagi kayak hubungan kita.  ')
+mengetik('											')
+mengetik('====================================							')
+mengetik('											')	
+mengetik('Aku rela ko jadi Processor yg kepanasan, asalkan kmu yg jadi heatsink'y yg setiap saat bisa mendinginkan ku	')
+mengetik('												')
+mengetik('=====================================										')	
+mengetik('								')	
+mengetik('Gak usah nyari celah xss deh, karena ketika kamu ngeklik hatiku udah muncul pop up nama kamu kok	')	
+mengetik('												')	 
+mengetik('=====================================								')
+mengetik('												')					
+mengetik('berharap setelah aku berhasil login di hati kamu ga akan ada tombol logout, dan sessionku ga bakal pernah expired.	')
+mengetik('												')		
+mengetik('=====================================							')	
+mengetik('														')	
+mengetik('Masa aku harus pake teknik symlink bypass buat buka2 folder hatimu yg open_basedir enabled.		')					
+mengetik('												')	
+mengetik('====================================									')	
+mengetik('										')
+mengetik('Diriku dan Dirimu itu ibarat PHP dan MySQL yang belum terkoneksi :| 			')
+mengetik('													')
+mengetik('=====================================				')	
+mengetik('														')
+mengetik('Jangan cuma bisa inject hatinya, tp harus bisa patchnya jg. Biar nggak selingkuh sama heker lain	')		
+mengetik('')
+mengetik('=====================================						')	
+mengetik('											')
+mengetik('Aku memang programmer PHP, tapi aku nggak akan php-in kamu kok.')	
+	
