3dccd2081581006981951c35d3ac1466a2074af6
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-06-28 22:08:52 +0700

    Create hacker-patah_hati.py

---
+++ hacker-patah_hati.py
@@ -0,0 +1,24 @@
+ import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik('   #HackerPatahHati
+Girl, meskipun aku lagi belajar PHP, aku gak akan php-in kamu
+Girl, tau gak PHP itu mudah dipelajarin, tapi kamu susah dilupain 
+Girl, kalo aku ping, kenapa timenya sampe 4 angka 
+Girl, aku pakai 8080 untuk menemuimu, 25 untuk menghubungimu tapi kenapa kamu menjawab error 406
+Girl, akhirnya aku gunakan 21, tapi kenapa error 401
+Girl, apa ip aku dibanned olehmu
+Girl, aku gak akan men-DOS lamu, karena aku pakai anonymity untuk sniffing kamu
+Girl, aku cemburu saat kamu didekati pria lain, butuh nitrocooler untuk mendinginkan hatiku
+Girl, biarpun kamu tidak acuh pdaku, tapi trojanku selalu memberi tau kalo ada malware berbahaya
+Girl, akhirnya aku tau, tak ada vurnerable untuk mendapatkan hatimu
+Girl, kuputuskan delete all about you, meski kusisakan satu bookmark tentangmu ')
