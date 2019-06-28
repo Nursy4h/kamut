8903938794acbe01d852119a586d9b788e285895
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-06-28 23:40:20 +0700

    Update and rename hacker-patah_hati.py to hacker-patah-hati.py

--- hacker-patah-hati.py
+++ hacker-patah-hati.py
@@ -1,4 +1,17 @@
- import random
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+import random
 import sys
 import time
 def mengetik(s):
@@ -10,8 +23,8 @@ def mengetik(s):
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
-mengetik('   #HackerPatahHati')
-mengetik('Girl, meskipun aku lagi belajar PHP, aku gak akan php-in kamu')
+mengetik(B'   #HackerPatahHati')
+mengetik(G'Girl, meskipun aku lagi belajar PHP, aku gak akan php-in kamu')
 mengetik('Girl, tau gak PHP itu mudah dipelajarin, tapi kamu susah dilupain ')
 mengetik('Girl, kalo aku ping, kenapa timenya sampe 4 angka ')
 mengetik('Girl, aku pakai 8080 untuk menemuimu, 25 untuk menghubungimu tapi kenapa kamu menjawab error 406')
