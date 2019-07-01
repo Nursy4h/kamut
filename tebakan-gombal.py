1e6bfef075d173c1cd464327237a77315169364a
NAZRIL <52010048+Nursy4h@users.noreply.github.com>
2019-07-01 17:41:21 +0700

    Create tebakan-gombal.py

---
+++ tebakan-gombal.py
@@ -0,0 +1,31 @@
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+mengetik(BB+'                   #tebakan-gombalan-pedas ')
+mengetik(GL+'   Malam paling indah ~ ')
+        time.sleep(random.random() * 0.5)
+mengetik(GG+'    MALAM APA YANG PALING INDAH???? ')
+mengetik(RR+' 1')
+mengetik(RR+' 2')
+mengetik(RR+' 3')
+mengetik(GG+'MELAMAR KAMU')
